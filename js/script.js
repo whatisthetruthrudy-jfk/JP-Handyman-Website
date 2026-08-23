0dd3035017f7dc8bfdeca365a5d685a9794400ec
Joe Anderson <whatisthetruthrudy@gmail.com>
2026-08-22 22:20:16 -0400

    Add initial website files for J @ P Handyman Services, including About, Contact, Gallery, Services pages, CSS styles, JavaScript functionality, and logo image.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// J @ P Handyman Services Website
+
+// Welcome message in browser console
+console.log("J @ P Handyman Services website loaded");
+
+
+// Contact form message
+function estimateMessage() {
+
+    alert(
+        "Thank you for contacting J @ P Handyman Services. We will get back to you soon!"
+    );
+
+}
